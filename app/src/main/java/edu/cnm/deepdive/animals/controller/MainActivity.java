9f9da404f028f08e2b61552d9902e8e29def08ef
darbach <danielarbach@gmail.com>
2021-02-22 12:03:18 -0700

    Replaced WebView with an ImageView + Picasso.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
@@ -1,67 +1,47 @@
-package edu.cnm.deepdive.animals;
+package edu.cnm.deepdive.animals.controller;
 
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.squareup.picasso.Picasso;
+import edu.cnm.deepdive.animals.BuildConfig;
+import edu.cnm.deepdive.animals.R;
 import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.service.WebServiceProxy;
 import java.io.IOException;
 import java.util.List;
-import java.util.Random;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-  private WebView contentView;
+  private ImageView image;
   private Spinner animalSelector;
-  private ArrayAdapter<Animal> adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    contentView = findViewById(R.id.content_view);
+    image = findViewById(R.id.image);
     animalSelector = findViewById(R.id.animal_selector); // our spinner in the .xml view
     animalSelector.setOnItemSelectedListener(new OnItemSelectedListener() {
       @Override
       public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         Animal animal = (Animal) parent.getItemAtPosition(position);
-        contentView.loadUrl(animal.getImageUrl());
-      }
+        Picasso.get().load(animal.getImageUrl()).into(image);      }
 
       @Override
       public void onNothingSelected(AdapterView<?> parent) {
         // Do nothing.
       }
     });
-    this.setupWebView();
-  }
-
-  private void setupWebView() {
-    //change behavior webviewclient so it doesn't try to open URL in an external browser
-    contentView.setWebViewClient(new WebViewClient() {
-      @Override
-      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-        return false;
-      }
-    });
-    WebSettings settings = contentView.getSettings();
-    settings.setSupportZoom(true);
-    settings.setBuiltInZoomControls(true);
-    settings.setDisplayZoomControls(false);
-    settings.setUseWideViewPort(true);
-    settings.setLoadWithOverviewMode(true);
-    new RetrieverTask().execute();
+    (new RetrieverTask()).execute();
   }
 
   private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
@@ -70,10 +50,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostExecute(List<Animal> animals) { // updating UI when background task done
       super.onPostExecute(animals);
       String url = animals.get(0).getImageUrl();
-      adapter = new ArrayAdapter<>(MainActivity.this,
+      ArrayAdapter<Animal> adapter = new ArrayAdapter<>(MainActivity.this,
           R.layout.item_animal_spinner, animals);
       adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-      contentView.loadUrl(url);
+//      image.loadUrl(url);
       animalSelector.setAdapter(adapter);
     }
 
